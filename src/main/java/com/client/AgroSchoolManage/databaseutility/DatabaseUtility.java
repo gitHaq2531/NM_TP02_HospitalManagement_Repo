4e78468d95473f4163de14cccecb3b21a8a45aaf
arjun <arjun@Chetak>
2025-10-10 16:16:38 +0530

    committed webdriverutility and databaseutility

---
+++ src/main/java/com/client/AgroSchoolManage/databaseutility/DatabaseUtility.java
@@ -0,0 +1,58 @@
+package com.client.AgroSchoolManage.databaseutility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.mysql.cj.jdbc.Driver;
+
+public class DatabaseUtility {
+	Connection conekt;
+	public void connectToDatabase() throws SQLException {
+		try {
+			Driver driver = new Driver();
+			DriverManager.registerDriver(driver);
+		 conekt =	DriverManager.getConnection("", "", "");
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+		
+	}
+	public ResultSet executeSelectQuery() throws SQLException {
+		ResultSet resultset=null;
+		try {
+		Statement state = conekt.createStatement();
+		 resultset = state.executeQuery("");
+		}catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return resultset;
+}
+	public int executeNonSelectQuery() {
+		int result=0;
+		try {
+			Statement state = conekt.createStatement();
+			 result = state.executeUpdate("");
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return result;
+	}
+	public void closeDatabaseConnection() throws SQLException {
+		try {
+			conekt.close();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		
+	}
+
+}
---
+++ src/main/java/com/client/AgroSchoolManage/webdriverutility/WebDriverUtility.java
@@ -0,0 +1,58 @@
+package com.client.AgroSchoolManage.webdriverutility;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WebDriverUtility {
+
+	public void maximize(WebDriver driver) {
+		driver.manage().window().maximize();
+
+	}
+
+	public void minimize(WebDriver driver) {
+		driver.manage().window().minimize();
+	}
+
+	public void waitForPageToLoad(WebDriver driver) {
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(25));
+	}
+
+	public void waitForElement(WebDriver driver, WebElement element) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(25));
+		wait.until(ExpectedConditions.elementToBeClickable(element));
+
+	}
+
+	public void selectDropDownVisibleText(WebElement element, String text) {
+		Select sel = new Select(element);
+		sel.selectByVisibleText(text);
+	}
+
+	public void selectDropDownByIndex(WebElement element, int index) {
+		Select sel = new Select(element);
+		sel.selectByIndex(index);
+	}
+
+	public void selectDropDownBy(WebElement element, String value) {
+		Select sel = new Select(element);
+		sel.selectByValue(value);
+	}
+
+	public void moveToAnElement(WebDriver driver, WebElement element) {
+		Actions action = new Actions(driver);
+		action.moveToElement(element).build().perform();
+	}
+
+	public void scrollToAnElement(WebDriver driver, WebElement element) {
+		Actions action = new Actions(driver);
+		action.scrollToElement(element);
+	}
+
+}
