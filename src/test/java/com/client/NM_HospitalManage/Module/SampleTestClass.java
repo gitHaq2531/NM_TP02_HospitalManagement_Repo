fd490da7b45edeb99c390e8e5b01e3dca89511ce
arjun <arjun@Chetak>
2025-10-11 11:27:57 +0530

    new class committed

--- src/test/java/com/client/NM_HospitalManage/Module/SampleTestClass.java
+++ src/test/java/com/client/NM_HospitalManage/Module/SampleTestClass.java
@@ -1,5 +1,11 @@
 package com.client.NM_HospitalManage.Module;
 
+import org.testng.annotations.Test;
+
 public class SampleTestClass {
+	@Test
+	public void demo() {
+		System.out.println("login test");
+	}
 
 }
