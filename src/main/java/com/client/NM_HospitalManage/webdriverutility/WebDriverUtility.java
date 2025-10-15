e41c9b66c7e45f8fd5ac7cc72d740f7f9855a517
HP <HP@DESKTOP-4HU8PPE>
2025-10-15 16:39:34 +0530

    commited

--- src/main/java/com/client/NM_HospitalManage/webdriverutility/WebDriverUtility.java
+++ src/main/java/com/client/NM_HospitalManage/webdriverutility/WebDriverUtility.java
@@ -5,7 +5,6 @@ import java.util.Set;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.WindowType;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
@@ -66,8 +65,7 @@ public class WebDriverUtility {
 			if(actTitle.contains(partialTitle)) {
 				return;
 			}
-			driver.switchTo().window(currentWindow);
-			
+			driver.switchTo().window(currentWindow);	
 		}
 	}
 
