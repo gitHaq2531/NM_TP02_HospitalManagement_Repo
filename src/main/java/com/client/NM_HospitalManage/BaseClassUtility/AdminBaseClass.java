1e2924b81e0f8b5fde36e3763cbe5207d9a04551
HP <HP@DESKTOP-4HU8PPE>
2025-10-15 12:18:42 +0530

    Added new method

--- src/main/java/com/client/NM_HospitalManage/BaseClassUtility/AdminBaseClass.java
+++ src/main/java/com/client/NM_HospitalManage/BaseClassUtility/AdminBaseClass.java
@@ -3,12 +3,31 @@ package com.client.NM_HospitalManage.BaseClassUtility;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import com.client.NM_HospitalManage.ObjectRepository.HmsAdminLoginPage;
+import com.client.NM_HospitalManage.ObjectRepository.MultipleLoginPage;
+
 public class AdminBaseClass extends BaseClass {
 
+	
+	
+	
 	@BeforeMethod
 	public void beforeMethodConfig() {
+		
+		MultipleLoginPage mlp = new MultipleLoginPage(driver);
+		HmsAdminLoginPage halp = new HmsAdminLoginPage(driver);
+		
 		System.out.println("login to admin portal");
-
+	
+		String USERNAME = System.getProperty("username", futils.getDataFromPropertyFile("username"));
+		String PASSWORD = System.getProperty("password", futils.getDataFromPropertyFile("password"));
+		
+		mlp.getAdminLink().click();
+		halp.getUserEdit().sendKeys(USERNAME);
+		halp.getPasswordEdit().sendKeys(PASSWORD);
+		halp.getLoginEdit().click();
+		
+		System.out.println("**********");
 	}
 
 	@AfterMethod
