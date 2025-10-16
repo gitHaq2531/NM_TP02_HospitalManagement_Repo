86c23bbd0c65ba343d692913f7ab0341e4520a38
abhimanyuray794-lgtm <abhimanyuray794@gmail.com>
2025-10-16 13:22:00 +0530

    added new printing statement

--- src/test/java/com/client/NM_HospitalManage/Module/DisplayAdminDashboardTest.java
+++ src/test/java/com/client/NM_HospitalManage/Module/DisplayAdminDashboardTest.java
@@ -15,6 +15,7 @@ public class DisplayAdminDashboardTest extends AdminBaseClass{
 		System.out.println("Hello there");
 		System.out.println("Hello11");
 		System.out.println("Hello THERE");
+		System.out.println("hello git bash.......");
 		
 
 
